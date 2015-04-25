6149481e25fa74ece498e2dbf5a51a23215df7d6
yj443035414 <443035414@qq.com>
2015-04-25 14:33:53 +0800

    abcdef
    abcdef

--- src/com/yj/Hello.java
+++ src/com/yj/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 		System.out.println("Hello Project!");
 		System.out.println("Hello Project!");
 		System.out.println("Hello Project!");
+		System.out.println("Hello Project!");
 	}
 }
